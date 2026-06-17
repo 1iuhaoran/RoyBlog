55e7548dd1b964f872e615dbbf0322e387b17c14
csz <chenzhangming5@gmail.com>
2026-06-17 14:33:45 +0800

    Initial commit

---
+++ backend/roybackend/src/main/java/com/roybackend/RoybackendApplication.java
@@ -0,0 +1,13 @@
+package com.roybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoybackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoybackendApplication.class, args);
+    }
+
+}
---
+++ backend/roybackend/src/test/java/com/roybackend/RoybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoybackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
